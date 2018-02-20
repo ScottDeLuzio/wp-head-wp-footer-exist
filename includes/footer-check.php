f18ca97841ba6cbc73305b7e59b0823ad62c1d9f
Scott DeLuzio <scott@surpriseazwebservices.com>
2018-02-20 12:03:34 -0700

    Fixed typo

--- includes/footer-check.php
+++ includes/footer-check.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_action( 'wp_footer', 'fsbgimg_check_wp_footer_exists' );
 function fsbgimg_check_wp_footer_exists(){
 	$current_user_can	= apply_filters( 'fsbgimg_current_user_can', 'manage_options' );
-	$display_text		= __( 'Congratulations! wp_footer() exists in your current theme.', 'wp-head-wp-footer-exist' );
+	$display_text		= __( 'Congratulations! wp_footer(); exists in your current theme.', 'wp-head-wp-footer-exist' );
 	if ( is_user_logged_in() && current_user_can( $current_user_can ) ){ ?>
 		<script type="text/javascript">
 			window.alert("<?php echo $display_text; ?>");
